a562be19c55b5509f5173b5b830de59c803d01c2
Min Latt <minminlaxz@gmail.com>
2021-07-26 15:06:27 +0630

    Testing updated workflows

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited in devel branch
+          Test for updated workflows.
         </p>
         <a
           className="App-link"
